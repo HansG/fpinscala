8cbfbea4d535720ed5e0254d910bc450526bcd7a
HansG <hans.gail@akdb.de>
2023-05-15 18:32:24 +0200

    @AKDB TypesTry.scala

--- src/test/scala/tries/TypesTry.scala
+++ src/test/scala/tries/TypesTry.scala
@@ -12,6 +12,7 @@ object TypesTry:
 
     extension (x: Logarithm)
       def toDouble: Double = math.exp(x)
+      def toString: Double = toDouble
       def +(y: Logarithm): Logarithm = Logarithm(math.exp(x) + math.exp(y))
       def *(y: Logarithm): Logarithm = x + y
 
@@ -23,6 +24,10 @@ object TypesTry:
     def tryPlusMal =
       val log2 = Logarithm(2.0)
       val log3 = Logarithm(3.0)
+      println(log2) // prints 6.0
+      println(log3) // prints 4.999...
+      println((log2).toDouble) // prints 6.0
+      println((log3).toDouble) // prints 4.999...
       println((log2 * log3).toDouble) // prints 6.0
       println((log2 + log3).toDouble) // prints 4.999...
 
@@ -40,6 +45,15 @@ object TypesTry:
       val age: Int
     }
 
+
+    type Book = Record1 {
+      val title: String
+      val author: String
+      val year: Int
+      val rating: Double
+    }
+
+
     class RecordTry:
       @Test
       def emma =
@@ -50,8 +64,51 @@ object TypesTry:
 
         println(s"${person.name} is ${person.age} years old.")
 
+      @Test
+      def rye =
+        val book = Record1(
+        "title" -> "The Catcher in the Rye",
+        "author" -> "J. D. Salinger",
+        "year" -> 1951,
+        "rating" -> 4.5
+        ).asInstanceOf[Book]
+        println(s"${book.title} is ${book.year} years old.")
 
 
+  end StructTypes
+
+
+  object DependantTypes:
+
+    trait Nums:
+      // the type of numbers is left abstract
+      type Num
+
+      // some operations on numbers
+      def lit(d: Double): Num
+      def add(l: Num, r: Num): Num
+      def mul(l: Num, r: Num): Num
+
+    object Doubles extends Nums:
+      type Num = Double
+
+      def lit(d: Double): Num = d
+      def add(l: Num, r: Num): Num = l + r
+      def mul(l: Num, r: Num): Num = l * r
+
+
+  //  type Prog = (n: Nums) => n.Num => n.Num
+    trait Prog :
+      def apply(n : Nums)(n.Num) : n.Num
+
+    val ex: Prog = nums => x => nums.add(nums.lit(0.8), x)
+
+    def derivative(input: Prog): Double = 0.8
+
+    derivative { nums => x => x }
+    derivative { nums => x => nums.add(nums.lit(0.8), x) }
+
+  end DependantTypes
 
 end TypesTry
 
